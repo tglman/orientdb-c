09d7b992bb8a438a66f74821c8d9f1c2dd97cfe7
tglman <tglman@tglman>
2010-11-07 18:38:20 +0100

    Improvement test of database url resolving.

--- test/test_o_url_resolver.c
+++ test/test_o_url_resolver.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <TestFramework/test.h>
 #include <stdio.h>
-#define TEST_URL "remote:127.0.0.1:3030/dbname"
+#define TEST_URL "remote:127.0.0.1:2424/dbname"
 
 void o_url_resolve_type_test()
 {
@@ -19,12 +19,24 @@ void o_url_resolve_information_test()
 	enum o_url_type type;
 	o_url_resolve_information(TEST_URL, &type, &path, &database);
 	assert_true(type == REMOTE, "Wrong url Type");
-	assert_true(strcmp(path, "127.0.0.1:3030")==0, "Wrong host name");
-	assert_true(strcmp(database, "dbname")==0, "Wrong database name");
+	assert_true(strcmp(path, "127.0.0.1:2424") == 0, "Wrong path ");
+	assert_true(strcmp(database, "dbname") == 0, "Wrong database name");
+	o_free(path);
+	o_free(database);
+}
+
+void o_url_resolve_host_port_from_path_test()
+{
+	char *host = o_malloc(512);
+	int port;
+	o_url_resolve_host_port_from_path("127.0.0.1:2424", &host, &port);
+	assert_true(strcmp(host, "127.0.0.1") == 0, "wrong host name");
+	assert_true(port == 2424, "wrong port");
 }
 
 void o_url_resolve_suite()
 {
 	ADD_TEST(o_url_resolve_type_test, "Test a resolve of type from url");
 	ADD_TEST(o_url_resolve_information_test, "Test a url resolver information");
+	ADD_TEST(o_url_resolve_host_port_from_path_test, "Test a host and port resolve from path");
 }
